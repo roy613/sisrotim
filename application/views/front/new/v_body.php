d74294c00e32599f40749ea1341c6bfe62e7dfdd
roy <royw50855@gmail.com>
2021-06-08 13:22:35 +0700

    tambahan v_kd

--- application/views/front/new/v_body.php
+++ application/views/front/new/v_body.php
@@ -1,37 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>SIMBADA | KUTAI TIMUR</title>
+    <link rel="apple-touch-icon" href="<?php echo base_url(); ?>assets/logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/logo.png">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/slick/slick.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/slick/slick-theme.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/tooplate-infinite-loop.css" />
+    <!--
+Tooplate 2117 Infinite Loop
+https://www.tooplate.com/view/2117-infinite-loop
+-->
+
+</head>
 <body>
     <!-- Hero section -->
     <section id="infinite" class="text-white tm-font-big tm-parallax">
-        <!-- Navigation -->
-        <!-- <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">              
-        <div class="container">   
-          <div class="tm-next">
-              <a href="#infinite" class="navbar-brand">Infinite Loop</a>
-          </div>             
-            
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars navbar-toggler-icon"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                  <a class="nav-link tm-nav-link" href="#infinite">Home</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link tm-nav-link" href="#whatwedo">What We Do</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link tm-nav-link" href="#testimonials">Testimonials</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link tm-nav-link" href="#gallery">Gallery</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link tm-nav-link" href="#contact">Contact</a>
-              </li>                    
-            </ul>
-          </div>        
-        </div>
-      </nav> -->
+       
 
         <div class="text-center tm-hero-text-container">
             <div class="tm-hero-text-container-inner">
@@ -79,19 +71,3 @@
                 </div>
             </div>
         </div>
-
-        <!-- <div class="row justify-content-center">
-            <div class="col-md-9">
-                <div class="tm-next tm-intro-next row d-flex justify-content-around">
-                    <a href="#whatwedo" class="col-md-3 tm-down-arrow-link">
-                        <i class="fas fa-3x fa-map-marked-alt tm-down-arrow"></i>
-                    </a>
-                    <a href="#whatwedo" class="col-md-3 tm-down-arrow-link">
-                        <i class="fas fa-3x fa-map-marked-alt tm-down-arrow"></i>
-                    </a>
-                    <a href="#whatwedo" class="col-md-3 tm-down-arrow-link">
-                        <i class="fas fa-3x fa-map-marked-alt tm-down-arrow"></i>
-                    </a>
-                </div>
-            </div>
-        </div> -->
--- application/views/front/new/v_header.php
+++ application/views/front/new/v_header.php
@@ -8,12 +8,13 @@
     <title>SIMBADA | KUTAI TIMUR</title>
     <link rel="apple-touch-icon" href="<?php echo base_url(); ?>assets/logo.png">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/logo.png">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/fontawesome-5.5/css/all.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/slick/slick.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/slick/slick-theme.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/magnific-popup/magnific-popup.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/tooplate-infinite-loop.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/f2/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/front1/css/tooplate-infinite-loop.css">
     <!--
 Tooplate 2117 Infinite Loop
 https://www.tooplate.com/view/2117-infinite-loop
@@ -21,3 +22,35 @@ https://www.tooplate.com/view/2117-infinite-loop
 
 </head>
 
+<body>
+    <!-- Hero section -->
+    <section id="infinite1" class="text-white tm-font-big tm-parallax">
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-md tm-navbar bg-dark" id="tmNav">
+            <div class="container">
+                <div class="tm-next">
+                    <a href="<?php echo base_url() . 'welcome' ?>" class="navbar-brand">
+                        <img src="<?php echo base_url(); ?>assets/logo.png" width="auto" height="40px" alt="">
+                        <span style="font-size:medium">SISTEM INFORMASI BATAS DESA</span>
+                    </a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars navbar-toggler-icon"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link tm-nav-link" href="#infinite">Kecamatan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link tm-nav-link" href="#whatwedo">Desa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link tm-nav-link" href="<?php echo base_url() . 'welcome' ?>">Kembali</a>
+                        </li>                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
